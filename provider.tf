4ade87802544e398a05f71134f15e15675499e3a
hieunc-woowa <chihieu.nguyen@vn.woowahan.com>
2021-05-21 15:59:01 +0700

    fix provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,15 @@
-# use local docker as provider
+# Set the required provider and versions
+terraform {
+  required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
+    docker = {
+      source  = "kreuzwerker/docker"
+    }
+  }
+}
 
+# use local docker as provider
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
